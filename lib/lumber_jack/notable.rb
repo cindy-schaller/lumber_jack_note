6a7389ed646cda767a61e02c34f76ef6cd2ceadd
Cindy Schaller <cindy@cswebconcepts.com>
2009-07-21 16:25:41 -0500

    add BKL fixes, add truncate and tests

--- lib/lumber_jack/notable.rb
+++ lib/lumber_jack/notable.rb
@@ -11,11 +11,11 @@ module LumberJack
         class_eval <<-EOV
           include LumberJack::Notable::InstanceMethods
           
-          def self.find_all_by_note(body)
+          def self.find_all_by_note(search)
             # Searches the notes table for the specified note body and returns 
             # an array of Notable objects
             na = []
-            Note.find(:all, :conditions => ["body = ?", body.to_s]).each do |t|
+            Note.find(:all, :conditions => ["title LIKE ? or body LIKE ?", search, search]).each do |t|
               na << t.notable_type.classify.constantize.find(t.notable_id)
             end
             na
@@ -24,26 +24,32 @@ module LumberJack
       end # def
     end # module
 
-    
-    
-    
     module InstanceMethods
-      def note_for(title)
+      def note_for(search)
         self.notes.find(
             :first, 
-            :conditions => ["title LIKE ?", "#{title}"], 
+            :conditions => ["title LIKE ? or body LIKE ?", search, search], 
             :order => 'position asc')
         end # def
       
-        def note_best
-          self.notes.find(:first, :order => 'position asc')
-        end
+      def note_best(body)
+        self.notes.find(:first, :order => 'position asc')
+      end
+    
+    
+      # def note_short_display(search)
+      #         self.notes.find(
+      #             :first, 
+      #             :conditions => ["title LIKE ? or body LIKE ?", search, search],
+      #             :order => 'position asc')
+      #       end # def
       
+            
       def method_missing(method_id, *arguments)
         if match = /note_for_([_a-zA-Z]\w*)/.match(method_id.to_s)
           self.notes.find(
             :first,
-            :conditions => ["body LIKE ?", "%#{$1.to_s.gsub('_', ' ')}%"],
+            :conditions => ["title LIKE ?", "%#{$1.to_s.gsub('_', ' ')}%"],
             :order => 'position asc')
         else
           super
@@ -52,6 +58,3 @@ module LumberJack
     end # module InstanceMethods
   end # module Notable
 end # module LumberJack
-
-
-
--- test/lumber_jack_note_test.rb
+++ test/lumber_jack_note_test.rb
@@ -2,6 +2,8 @@ require File.dirname(__FILE__) + '/test_helper'
 require 'test/unit'
 require 'active_record'
 require 'action_controller'
+
+
 class ApplicationController < ActionController::Base
 end
 # Note class requires ActsAsList to be installed as a plugin:
@@ -14,7 +16,7 @@ def setup_db
     create_table :organizations, :force => true do |t|
       t.string :name
     end
-    
+  
     create_table :notes, :force => true do |t|   
       t.string   :title
       t.string   :body
@@ -45,40 +47,48 @@ class LumberJackNoteTest < Test::Unit::TestCase
     (1..2).each { |i| Organization.create :name => "Organization #{i}"}
     @o1 = Organization.find(1)
     @o1.notes.build(:body => 'note 1', :title => 'title1')
-    @o1.notes.build(:body => 'note 2', :title => 'title2')
+    @o1.notes.build(:body => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at nisi in diam aliquet auctor. Duis laoreet pulvinar posuere.', :title => 'title2')
     @o1.save
     @o2 = Organization.find(2)
   end
   
   def teardown
-       teardown_db
+    teardown_db
   end
   
+  
   def test_create_and_count_organizations_and_notes
-      assert_equal 2, Organization.count
-      assert_equal 2, @o1.notes.count
-      assert_equal 0, @o2.notes.count
-    end
+    assert_equal 2, Organization.count
+    assert_equal 2, @o1.notes.count
+    assert_equal 0, @o2.notes.count
+  end
   
   def test_find_all_by_note
     assert_equal 'Organization 1', Organization.find_all_by_note('note 1')[0].name
     assert_equal 0, Organization.find_all_by_note('note test').size
     assert_equal 0, Organization.find_all_by_note(nil).size
   end
-
+  
+  
   def test_note_for
-    assert_equal 'note 1', @o1.note_for('title1').to_s
-    assert_equal '', @o2.note_for(nil).to_s
-  end  
+    assert_equal 'note 1', @o1.note_for('title1').display_note
+    # assert_equal 'Lorem ipsum dolor sit amet...', @o1.note_for('title2').truncate_note
+    # assert_equal '', @o2.note_for(nil).display_note
+  end      
   
   def test_note_for_method_missing
-    assert_equal 'note 1', @o1.note_for_title1.to_s
-    assert_equal '', @o1.note_for_titel3.to_s
-    assert_equal '', @o2.note_for_titel4.to_s
+    assert_equal 'note 1', @o1.note_for_title1.display_note 
+    assert_equal 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at nisi in diam aliquet auctor. Duis laoreet pulvinar posuere.', @o1.note_for_title2.display_note
+    assert_equal 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at nisi in diam aliquet auctor. Duis laoreet pulvinar posuere.', @o1.note_for_title2.truncate_note
+    # assert_equal '', @o1.note_for_title3.display_note
   end
   
+  # def test_note_short_display    
+  #     assert_equal 'Lorem ipsum dolor sit amet...', @o1.note_short_display('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at nisi in diam aliquet auctor. Duis laoreet pulvinar posuere.').truncate_note     
+  #   end
+  
+  
   def test_note_best
-    assert_equal 'note 1', @o1.note_best.to_s
+     assert_equal 'note 1', @o1.note_best(:body).display_note
   end
-
 end
